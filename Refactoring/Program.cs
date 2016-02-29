95a66b7dae30036b9650187769fea653593f7cf1
Alexander Tank <alexander.tank@comparex.de>
2016-02-29 11:29:53 +0100

    Vorbereitung für Workshop

--- Refactoring/Program.cs
+++ Refactoring/Program.cs
@@ -5,8 +5,6 @@ using Refactoring.Core;
 using Refactoring.Infrastructure;
 using Refactoring.Infrastructure.Clock;
 using Refactoring.Infrastructure.NHibernate;
-using Refactoring.MessageWay;
-using Refactoring.MessageWay.Decorators;
 using SimpleInjector;
 
 namespace Refactoring
@@ -15,6 +13,18 @@ namespace Refactoring
     {
         [STAThread]
         public static void Main()
+        {
+            var container = ConfigureContainer();
+
+            var orderService = container.GetInstance<IOrderService>();
+            orderService.PlaceOrder(new List<Item>
+            {
+                new Item {Name = "Schoki", Quantity = 100, Price = 1.99},
+                new Item {Name = "Zuckerli", Quantity = 13, Price = 0.99}
+            });
+        }
+
+        private static Container ConfigureContainer()
         {
             var container = new Container();
 
@@ -34,38 +44,17 @@ namespace Refactoring
             container.Register<IAuditingService, AuditingService>();
 
             // MessageWay
-            var assemblies = new[] { typeof(Program).Assembly };
-            container.Register<IDispatcher, Dispatcher>();
+            var assemblies = new[] {typeof (Program).Assembly};
+            //container.Register<IDispatcher, Dispatcher>();
 
-            container.Register(typeof(IMessageHandler<,>), assemblies);
+            container.Register(typeof (IMessageHandler<,>), assemblies);
 
             // Decorators
-            container.RegisterDecorator(typeof(IMessageHandler<,>), typeof(AuditingHandler<,>));
-            container.RegisterDecorator(typeof(IMessageHandler<,>), typeof(TransactionHandler<,>));
-            container.RegisterDecorator(typeof(IMessageHandler<,>), typeof(ExceptionHandler<,>));
-            container.RegisterDecorator(typeof(IMessageHandler<,>), typeof(LoggingHandler<,>));
-
+            // TODO
 
             container.Verify();
 
-            var dispatcher = container.GetInstance<IDispatcher>();
-
-            var basket = dispatcher.Dispatch(new PlaceOrderMessage
-            {
-                Items = new List<Item>
-                {
-                    new Item {Name = "Schoki", Quantity = 100, Price = 1.99},
-                    new Item {Name = "Zuckerli", Quantity = 13, Price = 0.99}
-                }
-            });
-
-            //var orderService = container.GetInstance<IOrderService>();
-
-            //orderService.PlaceOrder(new List<Item>
-            //{
-            //    new Item {Name = "Schoki", Quantity = 100, Price = 1.99},
-            //    new Item {Name = "Zuckerli", Quantity = 13, Price = 0.99}
-            //});
+            return container;
         }
     }
 
